7b442373aa96ffab65af6d2180b4a7eae529c2d3
ramprasath2511 <prasathram2511@gmail.com>
2022-01-25 21:00:33 +0530

    counter changed next

--- lib/main.dart
+++ lib/main.dart
@@ -78,11 +78,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: LoginScreen(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
